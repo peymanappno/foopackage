1421178d8f5d581fff64cc70f3ab33278b5dddc2
Peyman <peyman.seraj@appnovation.com>
2022-01-27 09:30:41 -0500

    fix

--- foopackage/barmodule.py
+++ foopackage/barmodule.py
@@ -1,5 +1,10 @@
 from __future__ import print_function
+from msilib.schema import Class
+from typing_extensions import Self
 
+class BarClass:
+    def __init__(self):
+        print('init BarClass')
 
-def barfunc():
-    print('barfunc')
+    def barfunc(self):
+        print('barfunc')
