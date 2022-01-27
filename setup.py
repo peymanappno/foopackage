f0c280207137b3e146626bcbcaba8a68f6c89085
Peyman <peyman.seraj@appnovation.com>
2022-01-27 12:36:56 -0500

    include non-py files in package

--- setup.py
+++ setup.py
@@ -55,6 +55,8 @@ setup(
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    package_data={'foopackage': ['*']},
+    include_package_data=True,
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
